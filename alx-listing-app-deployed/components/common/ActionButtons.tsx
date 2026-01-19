660f2aab81da6243f2db0c18f8cb637c9e522a01
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2026-01-19 04:59:26 +0300

    Fix formatting and import quotes in ActionButtons

--- alx-listing-app-deployed/components/common/ActionButtons.tsx
+++ alx-listing-app-deployed/components/common/ActionButtons.tsx
@@ -1,6 +1,6 @@
 // components/ActionButtons.tsx
-import React from "react";
-import { FunnelIcon } from "@heroicons/react/24/outline";
+import React from 'react';
+import { FunnelIcon } from '@heroicons/react/24/outline';
 
 const ActionButtons: React.FC = () => {
   return (
@@ -10,8 +10,7 @@ const ActionButtons: React.FC = () => {
         <FunnelIcon className="w-4 h-4" />
       </button>
       <button className="px-4 py-2 border border-[#E9E9E9] rounded-full text-sm text-[#161117] hover:text-white">
-        Sort by:{" "}
-        <span className="ml-1 font-medium text-[#161117]">Highest Price</span>
+        Sort by: <span className="ml-1 font-medium text-[#161117]">Highest Price</span>
       </button>
     </div>
   );
